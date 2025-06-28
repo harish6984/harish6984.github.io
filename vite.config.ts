93dbcd71b37fb18908908e7d7dcf528cf4fa2b4a
fw-harish <harish@farmerswife.com>
2025-06-28 11:45:18 +0200

    gh workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/harish6984.github.io/',
   server: {
     host: "::",
     port: 8080,
